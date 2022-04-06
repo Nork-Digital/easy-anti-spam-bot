c4aea0c03808f885c1a74d018e829ec2629a9d91
Caio Peres <caio@nork.digital>
2022-04-06 16:30:01 -0300

    1.0.2 fix msg and setting page

--- easy-anti-spam-bot.php
+++ easy-anti-spam-bot.php
@@ -1,24 +1,130 @@
 <?php
+
 /**
  * Plugin Name: Easy Anti Spam Bots
  * Plugin URI: https://github.com/cjperes/easy-anti-spam-bot
- * Description: Hide email from Spam Bots using a shortcode [email]your@email.com[/email]
+ * Description: Simple and 100% Plug-n-play - block spam bots, proxy users and more!
  * Author: Caio Peres
  * Author URI: https://github.com/cjperes
- * Version: 1.0.0
+ * Version: 1.0.2
  */
 
-
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // tela branca caso o plugin for acessado direto
+if (!defined('ABSPATH')) {
+    exit; // tela branca caso o plugin for acessado direto
 }
 
-// anti spam bot [email]your@email.com[/email] 
-function wpcodex_hide_email_shortcode( $atts , $content = null ) {
-	if ( ! is_email( $content ) ) {
-		return;
-	}
-	return '<a href="mailto:' . antispambot( $content ) . '">' . antispambot( $content ) . '</a>';
+// anti spam bot [email]your@email.com[/email]
+function easb_hide_email_shortcode($atts, $content = null)
+{
+    if (!is_email($content)) {
+        return;
+    }
+    return '<a href="mailto:' . antispambot($content) . '">' . antispambot($content) . '</a>';
 }
-add_shortcode( 'email', 'wpcodex_hide_email_shortcode' );
+add_shortcode('email', 'easb_hide_email_shortcode');
 
+
+
+
+//msg sucesso plugin ativo
+function easb_admin_notice__success()
+{
+    if (! PAnD::is_admin_notice_active('disable-done-notice-forever')) {
+        return;
+    }
+
+    ?>
+<div data-dismissible="disable-done-notice-forever" class="notice notice-success is-dismissible">
+<p><?php _e('Easy Anti Spam Bot Activated! Use the shortcode [email]your@email.com[/email] for protect the e-mail from bad bots. <a href="https://wordpress.org/support/plugin/easy-anti-spam-bots/reviews/#new-post" target="blank">If you like the plugin, consider rating the plugin with 5 stars ⭐⭐⭐⭐⭐, encourages us to add new features in the future!</a>'); ?></p>
+                                </div>
+    <?php
+}
+add_action('admin_init', array( 'PAnD', 'init' ));
+add_action('admin_notices', 'easb_admin_notice__success');
+
+
+
+
+//pagina plugin
+class AntiSpamBotSetting
+{
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
+
+    /**
+     * Start up
+     */
+    public function __construct()
+    {
+        add_action('admin_menu', array($this, 'easb_add_plugin_page'));
+        add_action('admin_init', array($this, 'page_init'));
+    }
+
+    /**
+     * Add options page
+     */
+    public function easb_add_plugin_page()
+    {
+        // This page will be under "Settings"
+        add_options_page(
+            'Anti Spam Bot', // page_title
+            'Anti Spam Bot', // menu_title
+            'manage_options', // capability
+            'e-anti-spam-bot', // menu_slug
+            array( $this, 'easb_create_admin_page' ) // function
+        );
+    }
+
+    /**
+     * Options page callback
+     */
+    public function easb_create_admin_page()
+    {
+        // Set class property
+        $this->options = get_option('easb_option_name');
+        ?>
+        <div class="wrap">
+            <h1>Easy Anti Spam Bot</h1>
+
+            <h3>How to use?</h3>
+
+            <p>Just insert the <strong>e-mail</strong> into the shortcode [email][/email]</p>
+            <p> <strong>Example: [email]your@email.com[/email]</strong> </p>
+            <p><a href="https://wordpress.org/support/plugin/easy-anti-spam-bots/reviews/#new-post" target="blank">If you like the plugin, consider rating the plugin with 5 stars ⭐⭐⭐⭐⭐, encourages us to add new features in the future!</a></p>
+        </div>
+        <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function page_init()
+    {
+        register_setting(
+            'easb_option_group', // Option group
+            'easb_option_name', // Option name
+            array($this, 'sanitize') // Sanitize
+        );
+
+        add_settings_section(
+            'setting_section_id', // ID
+            'Anti Spam bot', // Title
+            array($this, 'print_section_info'), // Callback
+            'easb-setting-admin' // Page
+        );
+
+        add_settings_field(
+            'id_number', // ID
+            'ID Number', // Title
+            array($this, 'id_number_callback'), // Callback
+            'easb-setting-admin', // Page
+            'setting_section_id' // Section
+        );
+    }
+}
+
+if (is_admin()) {
+    $easb_settings_page = new AntiSpamBotSetting();
+}
--- readme.txt
+++ readme.txt
@@ -1,7 +1,8 @@
 === Easy Anti Spam Bot ===
-Tags: anti, spam,bot, easy, light
+Contributors: cjperes
+Tags: anti, spam,bot, easy, light, block spam, anti spam, hide email
 Requires at least: 4.6
-Tested up to: 4.7
+Tested up to: 5.9.3
 Stable tag: 4.3
 Requires PHP: 5.2.4
 License: GPLv2 or later
@@ -13,9 +14,19 @@ Hide email from Spam Bots using a shortcode [email]your@email.com[/email]
 
 Hide email from Spam Bots using a shortcode [email]your@email.com[/email]
 
-Light and simple
+How the plugin Work?
+The email entered within the [email][/email] shortcode is camouflaged in the source code, thus preventing bad bots that scour the internet from being able to read and find your email.
+
+
 This plugin is based on standard functionality antispambot (https://codex.wordpress.org/Function_Reference/antispambot)
 Works perfectly with all themes and editors of Wordpress
+
+
+PT-BR
+O e-mail inserido dentro do shortcode [email][/email] é camuflado no código fonte, impedindo assim que bad bots que vasculham a internet consigam ler e encontrar o seu e-mail.
+
+
+Icon made by Material and Awesome Icons
 
 
 == Installation ==
@@ -27,3 +38,14 @@ This section describes how to install the plugin and get it working.
 3. Use the shortcode tags in any editor [email]your@email.com[/email]
 4. Done
 
+
+== Changelog ==
+	
+= 1.0.0 - 2018/08/19 =
+- Lançamento do plugin.
+
+= 1.0.1 - 2022/01/14 =
+- Página com instruções de uso
+
+= 1.0.2 - 2022/04/06 =
+- Correção mensagem de plugin ativo não era removida ao fechar.
